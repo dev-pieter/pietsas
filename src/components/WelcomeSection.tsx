7a846e2e6a80dadd2e1fa572dcc11eecf1b77f96
dev-pieter <pieter_nortje@outlook.com>
2025-02-27 08:29:58 +0200

    wip: Adjust home page text size and change home bg image

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,10 +1,10 @@
 const WelcomeSection = () => {
   return (
-    <section className="text-center max-w-3xl mx-auto">
-      <h2 className="text-4xl font-bold mb-6 text-primary">
+    <section className="max-w-3xl mx-auto">
+      <h2 className="text-center text-2xl font-bold mb-6 text-primary">
         Welcome to the Nortjé Wedding
       </h2>
-      <p className="text-lg text-muted-foreground">
+      <p className="text-muted-foreground">
         We are thrilled to invite you to celebrate our special day with us. Join
         us as we begin our journey together surrounded by our beloved family and
         friends.
